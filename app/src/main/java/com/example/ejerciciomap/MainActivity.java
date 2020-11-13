8c0aea206c60dfbbd15ddad07a95d9e08c3e38c6
oscar salazar <salazaroscar9705@gmail.com>
2020-11-13 18:02:37 -0500

    activando el gps y obtener coodernadaas

--- app/src/main/java/com/example/ejerciciomap/MainActivity.java
+++ app/src/main/java/com/example/ejerciciomap/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.ejerciciomap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.mapbox.mapboxsdk.Mapbox;
@@ -10,6 +17,10 @@ import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private MapView mapView;
@@ -20,20 +31,22 @@ public class MainActivity extends AppCompatActivity {
         Mapbox.getInstance(this, getString(R.string.acces_token));
         setContentView(R.layout.activity_main);
 
-        mapView =(MapView) findViewById(R.id.mapView);
+        // LocationManager obtain GPS location
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        MyLocationListener MLocationListener = new MyLocationListener();
+        mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 mapboxMap.addMarker(new MarkerOptions()
-                .position(new LatLng(3.449500,-76.534004))
-                .title("UAD")
-                .snippet("ubicacion san fernando"));
+                        .position(new LatLng(3.449500, -76.534004))
+                        .title("UAD")
+                        .snippet("ubicacion san fernando"));
 
 
-
-        }
+            }
         });
     }
 
@@ -48,21 +61,25 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         mapView.onStop();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         mapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         mapView.onPause();
     }
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -73,5 +90,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+    public void setLocation(Location location){
+        if(location.getLatitude() != 0.0 && location.getLongitude()!= 0){
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                  location.getLatitude(), location.getLongitude(),1);
+                if(!list.isEmpty()){
+                    Address address = list.get(0);
+
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class MyLocationListener  implements  LocationListener{
+
+    MainActivity mainActivity;
+
+    public MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    public void setMainActivity(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        location.getLatitude();
+        location.getLongitude();
+        String text = "Mi ubicación actual es: " +
+                "\n Lat = " + location.getLatitude()+
+                "\n Log = " + location.getLongitude();
+        this.mainActivity.setLocation(location);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 }
